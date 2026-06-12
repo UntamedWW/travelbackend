ef1a0f0e7e96a83371b516f13f380cc3253e9abf
Yakoreva Sofjia <jakorevasofija16@gmail.com>
2026-06-12 12:25:17 -0500

    Added registration;

--- src/main/java/com/travel/travelbackend/TravelController.java
+++ src/main/java/com/travel/travelbackend/TravelController.java
@@ -3,7 +3,7 @@ package com.travel.travelbackend;
 import com.travel.travelbackend.entity.User;
 import com.travel.travelbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TravelController {
     private final UserService userService;
 
+    @PostMapping("/register")
     public User registration(@RequestBody User user){
         return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.login(user);
     }
 }
--- src/main/java/com/travel/travelbackend/service/UserService.java
+++ src/main/java/com/travel/travelbackend/service/UserService.java
@@ -3,7 +3,6 @@ package com.travel.travelbackend.service;
 import com.travel.travelbackend.entity.User;
 import com.travel.travelbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,9 +12,16 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User register(User user){
-        if(user.getPassword() == null || user.getPassword().isBlank() || user.getPassword().length() <= 4) throw new IllegalArgumentException("Password must be at least 4 characters");
+        if(user.getPassword() == null ||
+                user.getPassword().isBlank() ||
+                user.getPassword().length() <= 4) {
+            throw new IllegalArgumentException("Password must be at least 4 characters");
+        }
 
-        if(user.getEmail() == null || user.getEmail().isBlank()) throw new IllegalArgumentException("Email is required");
+        if(user.getEmail() == null ||
+                user.getEmail().isBlank()) {
+            throw new IllegalArgumentException("Email is required");
+        }
 
         User existingUser = userRepository.findByEmail(user.getEmail());
 
@@ -23,5 +29,17 @@ public class UserService {
 
         return userRepository.save(user);
 
+    }
+
+    public User login(User user){
+        User existingUser = userRepository.findByEmail(user.getEmail());
+
+        if(existingUser == null) throw new IllegalArgumentException("Invalid email or password");
+
+        if(!user.getPassword().equals(existingUser.getPassword())){
+            throw  new IllegalArgumentException("Invalid email or password");
+        }
+
+        return existingUser;
     }
 }
